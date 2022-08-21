6c1bad9eab7b872244076e55a7ac1fe6155c52fc
= <=>
2022-08-21 16:13:30 +0700

    tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TailwindComponent } from './tailwind/tailwind.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tailwind', component: TailwindComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TailwindComponent } from './tailwind/tailwind.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TailwindComponent
   ],
   imports: [
     BrowserModule,
